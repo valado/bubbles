5bb1e48039d9186595c96d5187c081f268620a8c
Vladimir Daskalov <vld.daskalov@gmail.com>
2025-01-09 00:40:40 +0100

    feat: working example

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,34 @@ import { InteractiveGradient } from "../lib/InteractiveGradient";
 
 const App: FC = () => {
   return (
-    <div
-      style={{
-        top: "50%",
-        left: "50%",
-        position: "absolute",
-        transform: "translate(-50%, -50%)",
-      }}
-    >
-      <InteractiveGradient size={"500px"} morph float>
-        <span>test</span>
-      </InteractiveGradient>
-    </div>
+    <>
+      <div
+        style={{
+          top: "-50vh",
+          right: "-30vw",
+          position: "absolute",
+        }}
+      >
+        <InteractiveGradient size={"85vw"} morph />
+      </div>
+
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          right: "50%",
+          transform: "translate(50%, -50%)",
+        }}
+      >
+        <InteractiveGradient size={"10vw"} morph float>
+          <span
+            style={{ color: "white", fontWeight: "bold", userSelect: "none" }}
+          >
+            Dynamic Gradient
+          </span>
+        </InteractiveGradient>
+      </div>
+    </>
   );
 };
 
